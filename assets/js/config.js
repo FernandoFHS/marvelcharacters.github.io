3822a21737dfd04dff6e8122cf5b4fb5abd72f15
Fernando Henrique da Silva <fernando.silva@fmxsolucoes.com.br>
2022-02-14 09:08:50 -0300

    Updates

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
 angular.module("ENV", [])
-.constant("ENV", {"redirectURI":"http://production-app.co/"});
+.constant("ENV", {"redirectURI":"https://fernandofhs.github.io/marvelcharacters.github.io/"});
